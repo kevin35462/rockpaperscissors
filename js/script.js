eb23a2b7d455fe5bd47365641367d0fcbc973db3
Kevin Torres <kevintorres35462@gmail.com>
2018-11-28 22:55:37 +0000

    RPS

--- js/script.js
+++ js/script.js
@@ -7,13 +7,19 @@
 var userChoice="";
 var computerChoice="";
 var winner="";
-var randomNumber=0;
+var randomNumber=Math.random();
 
 
 // DOCUMENT READY FUNCTION BELOW
 $("#shoot").click(function(){
     userChoice = $("input").val();
     $("#userChoice").text(userChoice);
-    
+    if(randomNumber<=.33){
+        computerChoice="rock"
+    }
+    else if(randomNumber<=.66)
+        computerChoice="paper"
+    else if(randomNumber<=.99)
+        computerChoice="scissors"
+        
 });
-
