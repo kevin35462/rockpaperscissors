583a237198a984ac063ad569c1fed02cc81a6d1b
Kevin Torres <kevintorres35462@gmail.com>
2019-03-01 02:10:07 +0000

    d

--- js/script.js
+++ js/script.js
@@ -8,6 +8,7 @@ var userChoice = "";
 var computerChoice = "";
 var winner = "";
 var randomNumber = 0;
+var input = document.getElementById("myInp");
 
 // DOCUMENT READY FUNCTION BELOW
 $(document).ready(function() {
@@ -16,7 +17,7 @@ $(document).ready(function() {
         
         randomNumber = Math.random();
         console.log(randomNumber);
-        userChoice = $("input").val();
+        userChoice = $("#myInp").val().toLowerCase();
         $("#userChoice").html("<h1>"+userChoice+"</h1>");
         
         if(randomNumber<=.33){
@@ -28,34 +29,43 @@ $(document).ready(function() {
         else{
             computerChoice="scissors";
         }
-        
         if(userChoice===computerChoice){
-            $("#result").html("<h1>Tie</h1>");
+            $("#result").html("<h1>Nobody Wins</h1>");
         }
          else if(userChoice==="rock"||userChoice==="Rock"){
             if(computerChoice==="scissors"){
-                $("#result").html("<h1>Winner</h1>");
+                $("#result").html("<h1>User Wins</h1>");
             }
                 else{
-                    $("#result").html("<h1>loser</h1>");
+                    $("#result").html("<h1>Computer Wins</h1>");
         }
         }
          else if(userChoice==="paper"){
             if(computerChoice==="rock"){
-                $("#result").html("<h1>Winner</h1>");
+                $("#result").html("<h1>User Wins</h1>");
             }
                 else{
-                    $("#result").html("<h1>loser</h1>");
+                    $("#result").html("<h1>Computer Wins</h1>");
         }
         }
          else if(userChoice==="scissors"){
             if(computerChoice==="paper"){
-                $("#result").html("<h1>Winner</h1>");
+                $("#result").html("<h1>User Wins</h1>");
             }
                 else{
-                    $("#result").html("<h1>loser</h1>");
+                    $("#result").html("<h1>Computer Wins</h1>");
         }
         }
     $("#computerChoice").html("<h1>"+computerChoice+"</h1>");
+    if(userChoice===""){
+        $("#result").html("<h1>Please Write rock , paper , or scissors</h1>");
+        $("#computerChoice").html("");
+        }
     });
+});
+input.addEventListener("keyup", function(event) {
+    event.preventDefault();
+    if (event.keyCode === 13) {
+        document.getElementById("shoot").click();
+    }
 });
